d0af0591c2af2ea6e1a5e216b541c144bd7f38e4
kochan0802 <kochan19920802@gmail.com>
2023-03-29 15:28:22 +0000

    init

--- resources/views/admin/auth/register.blade.php
+++ resources/views/admin/auth/register.blade.php
@@ -6,7 +6,7 @@
 
         <!-- Name -->
         <div>
-            <x-input-label for="name" :value="__('Name')" />
+            <x-input-label for="name" :value="__('お名前')" />
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
@@ -20,7 +20,7 @@
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label for="password" :value="__('パスワード')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
@@ -32,7 +32,7 @@
 
         <!-- Confirm Password -->
         <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+            <x-input-label for="password_confirmation" :value="__('パスワードの確認')" />
 
             <x-text-input id="password_confirmation" class="block mt-1 w-full"
                             type="password"
@@ -40,16 +40,14 @@
 
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
-
-            <!-- License -->
-            <div class="mt-4">
-                <x-input-label for="license" :value="__('資格')" />
-            
-                <x-textarea-input id="license" class="block mt-1 w-full"
+    
+    <!-- License -->
+    　<div class="mt-4">
+        <x-input-label for="license" :value="__('カウンセリング資格')" />
+        <x-textarea-input id="license" class="block mt-1 w-full"
                                 name="license"
                                 required autocomplete="license">{{ old('license') }}</x-textarea-input>
-            
-                <x-input-error :messages="$errors->get('license')" class="mt-2" />
+        <x-input-error :messages="$errors->get('license')" class="mt-2" />
             </div>
             
         <!-- Career -->
@@ -62,52 +60,62 @@
         
         <x-input-error :messages="$errors->get('career')" class="mt-2" />
         </div>
+    
+        <!--  16 personalities -->
+               
+         <div class="mt-4">
+            <x-input-label for="personalities" :value="__('16personalities 診断結果')" />
         
-        
-        <!--  ability_type -->
+            <select id="personalities" name="personalities" class="block mt-1 w-full">
+               
+        <option value="">-- Personalities --</option>
+        <optgroup label="分析家">
+        <option value="建築家" {{ old('personalities') === '建築家' ? 'selected' : '' }}>建築家</option>
+        <option value="論理学者" {{ old('personalities') === '論理学者' ? 'selected' : '' }}>論理学者</option>
+        <option value="指揮官" {{ old('personalities') === '指揮官' ? 'selected' : '' }}>指揮官</option>
+        <option value="討論者" {{ old('personalities') === '討論者' ? 'selected' : '' }}>討論者</option>
+        <optgroup label="外交官">
+            <option value="提唱者" {{ old('personalities') === '提唱者' ? 'selected' : '' }}>提唱者</option>
+            <option value="仲介者" {{ old('personalities') === '仲介者' ? 'selected' : '' }}>仲介者</option>
+            <option value="主人公" {{ old('personalities') === '主人公' ? 'selected' : '' }}>主人公</option>
+            <option value="広報運動家" {{ old('personalities') === '広報運動家' ? 'selected' : '' }}>広報運動家</option>
+        </optgroup>
+        <optgroup label="番人">
+            <option value="管理者" {{ old('personalities') === '管理者' ? 'selected' : '' }}>管理者</option>
+            <option value="擁護者" {{ old('personalities') === '擁護者' ? 'selected' : '' }}>擁護者</option>
+            <option value="幹部" {{ old('personalities') === '幹部' ? 'selected' : '' }}>幹部</option>
+            <option value="領事館" {{ old('personalities') === '領事館' ? 'selected' : '' }}>領事館</option>
+        </optgroup>
+        <optgroup label="探検家">
+            <option value="巨匠" {{ old('personalities') === '巨匠' ? 'selected' : '' }}>巨匠</option>
+            <option value="冒険者" {{ old('personalities') === '冒険者' ? 'selected' : '' }}>冒険者</option>
+            <option value="起業家" {{ old('personalities') === '起業家' ? 'selected' : '' }}>起業家</option>
+        </optgroup>
+                </select>
+                <x-input-error :messages="$errors->get('personalities')" class="mt-2" />
+                </div>
+                
+   <!--ability_type -->
     <div class="mt-4">
     <x-input-label for="category_id" :value="__('得意ジャンル')" />
 
-    <select id="category_id" name="personalities" class="block mt-1 w-full">
+    <select id="category_id" name="category_id" class="block mt-1 w-full">
    
     <option value="">-- Strong asset  --</option>   
-    <option value="建築家" {{ old('personalities') === '建築家' ? 'selected' : '' }}>建築家</option>
-       
-       
-    <div class="mt-4">
-    <x-input-label for="personalities" :value="__('性格診断結果')" />
-
-    <select id="personalities" name="personalities" class="block mt-1 w-full">
-       
-<option value="">-- Personalities --</option>
-<optgroup label="分析家">
-<option value="建築家" {{ old('personalities') === '建築家' ? 'selected' : '' }}>建築家</option>
-<option value="論理学者" {{ old('personalities') === '論理学者' ? 'selected' : '' }}>論理学者</option>
-<option value="指揮官" {{ old('personalities') === '指揮官' ? 'selected' : '' }}>指揮官</option>
-<option value="討論者" {{ old('personalities') === '討論者' ? 'selected' : '' }}>討論者</option>
-<optgroup label="外交官">
-    <option value="提唱者" {{ old('personalities') === '提唱者' ? 'selected' : '' }}>提唱者</option>
-    <option value="仲介者" {{ old('personalities') === '仲介者' ? 'selected' : '' }}>仲介者</option>
-    <option value="主人公" {{ old('personalities') === '主人公' ? 'selected' : '' }}>主人公</option>
-    <option value="広報運動家" {{ old('personalities') === '広報運動家' ? 'selected' : '' }}>広報運動家</option>
-</optgroup>
-<optgroup label="番人">
-    <option value="管理者" {{ old('personalities') === '管理者' ? 'selected' : '' }}>管理者</option>
-    <option value="擁護者" {{ old('personalities') === '擁護者' ? 'selected' : '' }}>擁護者</option>
-    <option value="幹部" {{ old('personalities') === '幹部' ? 'selected' : '' }}>幹部</option>
-    <option value="領事館" {{ old('personalities') === '領事館' ? 'selected' : '' }}>領事館</option>
-</optgroup>
-<optgroup label="探検家">
-    <option value="巨匠" {{ old('personalities') === '巨匠' ? 'selected' : '' }}>巨匠</option>
-    <option value="冒険者" {{ old('personalities') === '冒険者' ? 'selected' : '' }}>冒険者</option>
-    <option value="起業家" {{ old('personalities') === '起業家' ? 'selected' : '' }}>起業家</option>
-</optgroup>
-        </select>
-        <x-input-error :messages="$errors->get('personalities')" class="mt-2" />
-        </div>
-        
-        
-    <!-- URL -->
+    <option value="1" {{ old('category_id') === '1' ? 'selected' : '' }}>やりたいこと探し</option>
+    <option value="2" {{ old('category_id') === '2' ? 'selected' : '' }}>起業</option>
+    <option value="3" {{ old('category_id') === '3' ? 'selected' : '' }}>副業</option>
+    <option value="4" {{ old('category_id') === '4' ? 'selected' : '' }}>資格取得</option>
+    <option value="5" {{ old('category_id') === '5' ? 'selected' : '' }}>パラレルワーク</option>
+    <option value="6" {{ old('category_id') === '6' ? 'selected' : '' }}>クリエイター</option>
+    <option value="7" {{ old('category_id') === '7' ? 'selected' : '' }}>自分に合った働き方</option>
+    <option value="8" {{ old('category_id') === '8' ? 'selected' : '' }}>趣味</option>
+    <option value="9" {{ old('category_id') === '9' ? 'selected' : '' }}>海外移住</option>
+    <option value="10" {{ old('category_id') === '10' ? 'selected' : '' }}>その他</option>
+     <x-input-error :messages="$errors->get('career')" class="mt-2" />
+    </div>
+            
+     <!--URL -->
   <div class="mt-4">
                 <x-input-label for="url" :value="__('アイテマスURL')" />
             
@@ -121,18 +129,20 @@
  
 <!-- プロフィール画像アップロード -->
         <div class="mt-4">
-    <x-input-label for="profile_image" :value="__('Profile Image')" />
+    <x-input-label for="profile_image" :value="__('プロフィール画像登録')" />
     <input id="profile_image" type="file" name="profile_image" />
     <x-input-error :messages="$errors->get('profile_image')" class="mt-2" />
 </div>
    
+ 
+   
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
+                {{ __('すでに登録されていますか?') }}
             </a>
 
             <x-primary-button class="ml-4">
-                {{ __('Register') }}
+                {{ __('登録') }}
             </x-primary-button>
         </div>
     </form>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,7 +4,7 @@
 
         <!-- Name -->
         <div>
-            <x-input-label for="name" :value="__('Name')" />
+            <x-input-label for="name" :value="__('名前')" />
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
@@ -18,7 +18,7 @@
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label for="password" :value="__('パスワード')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
@@ -30,7 +30,7 @@
 
         <!-- Confirm Password -->
         <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+            <x-input-label for="password_confirmation" :value="__('パスワードの確認')" />
 
             <x-text-input id="password_confirmation" class="block mt-1 w-full"
                             type="password"
@@ -41,7 +41,7 @@
         
         <!-- Birthday -->
          <div class="mt-4">
-        <x-input-label for="birthday" :value="__('Birthday')" />
+        <x-input-label for="birthday" :value="__('生年月日')" />
         <x-text-input id="birthday" class="block mt-1 w-full" type="date" name="birthday" :value="old('birthday')" required />
         <x-input-error :messages="$errors->get('birthday')" class="mt-2" />
     </div>
@@ -49,7 +49,7 @@
         
         <!-- Gender -->
          <div class="mt-4">
-    <x-input-label for="gender" :value="__('Gender')" />
+    <x-input-label for="gender" :value="__('性別')" />
 
     <select id="gender" name="gender"
             class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow-sm text-gray-700 leading-tight focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:bg-white focus:outline-none">
@@ -62,7 +62,7 @@
     
     <!-- Personality_type -->
     <div class="mt-4">
-    <x-input-label for="personalities" :value="__('性格診断結果')" />
+    <x-input-label for="personalities" :value="__('16personalities 診断結果')" />
 
     <select id="personalities" name="personalities" class="block mt-1 w-full">
        
@@ -96,7 +96,7 @@
         
         <!-- プロフィール画像アップロード -->
         <div class="mt-4">
-    <x-input-label for="profile_image" :value="__('Profile Image')" />
+    <x-input-label for="profile_image" :value="__('プロフィール画像登録')" />
     <input id="profile_image" type="file" name="profile_image" />
     <x-input-error :messages="$errors->get('profile_image')" class="mt-2" />
 </div>
